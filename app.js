7817029a155dafa01dd9f74b6807ba2865f83e94
Tarun Suresh <tarunsuresh.v@gmail.com>
2023-05-21 22:14:19 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,29 +1,67 @@
-const express = require('express')
-const sqlite3 = require('sqlite3')
-const {open} = require('sqlite')
-const path = require('path')
-const app = express()
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const {open} = require('sqlite');
+const jwt = require('jsonwebtoken')
+const path = require('path');
+const { log } = require('console');
 
-const dbPath = path.join(__dirname, 'test.db')
-let database = null
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, 'test.db');
+let db = null;
 
 const initializeServerAndDb = async () => {
     try {
-        database = await open({
+        db = await open({
             filename: dbPath,
             driver: sqlite3.Database
-        })
+        });
 
         app.listen(3000, (req, res) => {
             console.log("Server started on port 3000");
-        })
+        });
     } catch(e) {
         console.log(`DB Error: ${e.message}`);
     }
 }
 
-initializeServerAndDb()
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
+
+initializeServerAndDb();
+
+const authenticateUser = (req, res, next) => {
+    const {authorization} = req.headers;
+    const token = authorization.split(" ")[1];
+    
+    if (token === undefined) {
+        res.status(401);
+        res.send({status_code: 401, error_msg: 'Invalid User Credentials'});
+    } else {
+        jwt.verify(token, 'TARUN_SURESH_06', async (error, payload) => {
+            if (error) {
+                res.status(401);
+                res.send({status_code: 401, error_msg: 'Invalid User Credentials'});
+            } else {
+                req.payload = payload;
+                next();
+            }
+        })
+    }
+}
+
+app.post('/register', (req, res) => {
+    const { username, password, name } = req.body;
+    const insertRegisterDataQuery = `
+    INSERT INTO user_details
+    (name, username, password) 
+    VALUES
+    (${name}, ${username}, ${password});
+    `;
+
+
+})
+
+app.post('/', authenticateUser, (req, res) => {
+    res.send("Hello World");
 });
